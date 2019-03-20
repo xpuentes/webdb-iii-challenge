02e8f5035c78ea3ffeef07c8764c43d933a6fe01
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-19 21:38:57 -0500

    Added [GET] /api/cohorts/:id endpoint

--- index.js
+++ index.js
@@ -21,5 +21,23 @@ server.get('/api/cohorts', (req, res) => {
     });
 });
 
+server.get('/api/cohorts/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cohorts')
+    .where({id: id})
+    .first()
+    .then(cohorts => {
+      if(cohorts){
+        res.status(200).json(cohorts);
+      } else {
+        res.status(400).json({message: 'Cohort does not exist.'})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error loading data'});
+    });
+});
+
 const port = process.env.PORT || 9090;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
