689ff06873307c2fecd5e6cd9162d55894bfe3ca
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-03-19 21:37:11 -0500

    Added [GET] /api/cohorts endpoint

--- index.js
+++ index.js
@@ -11,5 +11,15 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
+server.get('/api/cohorts', (req, res) => {
+  db('cohorts')
+    .then(cohorts => {
+      res.status(200).json(cohorts);
+    })
+    .catch(err => {
+      res.status(500).json({message: 'Error loading data.'});
+    });
+});
+
 const port = process.env.PORT || 9090;
 server.listen(port, () => console.log(`\nrunning on ${port}\n`));
